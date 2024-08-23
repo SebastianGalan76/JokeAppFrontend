f65f84b5fdaa7696951b3f6332a9ced39834348c
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-08-23 20:11:42 +0200

    Added ability to toggle the aside menu on small screens

--- src/app/home-page/aside-menu/aside-menu.component.ts
+++ src/app/home-page/aside-menu/aside-menu.component.ts
@@ -1,19 +1,27 @@
 import { Component, QueryList, ViewChildren } from '@angular/core';
 import { ButtonComponent } from './button/button.component';
+import { ToggleMenuButtonComponent } from './toggle-menu-button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-aside-menu',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ToggleMenuButtonComponent, CommonModule],
   templateUrl: './aside-menu.component.html',
   styleUrl: './aside-menu.component.scss'
 })
 export class AsideMenuComponent {
   @ViewChildren(ButtonComponent) navButtons!: QueryList<ButtonComponent>;
 
+  isShown: boolean = false;
+
   handleButtonSelect(selectedButton: ButtonComponent): void {
     this.navButtons.forEach(button => {
       button.isSelected = button === selectedButton;
     });
+  }
+
+  handleToggleMenu() : void {
+    this.isShown = !this.isShown;
   }
 }
---
+++ src/app/home-page/aside-menu/toggle-menu-button.component.ts
@@ -0,0 +1,60 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle-menu-button',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div id="toggle-menu-button" (click)="toggleMenu()">
+        <i [ngClass]="icon"></i>
+    </div>
+  `,
+  styles: `
+    #toggle-menu-button {
+        position: absolute;
+        right: 12px;
+        top: 10px;
+
+        color: #646464;
+
+        width: 30px;
+        height: 30px;
+
+        display: none;
+        align-items: center;
+        justify-content: center;
+
+        font-size: 1.2em;
+        font-weight: 500;
+
+        flex-shrink: 0;
+    }
+
+    @media (max-width: 600px) {
+        #toggle-menu-button{
+            display: flex;
+        }
+    }
+  `,
+})
+export class ToggleMenuButtonComponent {
+  @Input() isShown: boolean = false;
+
+  @Output() clickButton = new EventEmitter<void>();
+
+  icon: string = "fa-solid fa-square-caret-right";
+
+  toggleMenu(){
+    this.clickButton.emit();
+    
+    this.isShown = !this.isShown;
+
+    if(this.isShown){
+      this.icon = "fa-solid fa-square-caret-left";
+    }
+    else{
+      this.icon = "fa-solid fa-square-caret-right";
+    }
+  }
+}
