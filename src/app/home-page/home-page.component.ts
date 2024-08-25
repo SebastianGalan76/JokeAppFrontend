8e5b0168d8bcdee1650b13721bb477af34893934
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-08-25 10:12:30 +0200

    Created main header

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { AsideMenuComponent } from "./aside-menu/aside-menu.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
-  imports: [AsideMenuComponent]
+  imports: [AsideMenuComponent, HeaderComponent]
 })
 export class HomePageComponent {
 
